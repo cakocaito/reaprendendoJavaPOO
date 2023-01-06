e48120bb39666dee4243a19e4c930dc8366f0c33
cakocaito <cmsssilva@id.uff.br>
2023-01-06 15:12:17 -0300

    projeto iniciado

---
+++ src/main/java/com/mycompany/reaprendendojavapoo/ReaprendendoJavaPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reaprendendojavapoo;
+
+/**
+ *
+ * @author cakoc
+ */
+public class ReaprendendoJavaPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
